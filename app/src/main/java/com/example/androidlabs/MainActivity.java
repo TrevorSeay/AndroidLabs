74015532dd55b030b8a9aa02f9f342cbf11af0a4
Trevor <trevorghseay@gmail.com>
2020-09-27 15:02:02 -0400

    step 1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+
+        findViewById(R.id.button).setOnClickListener(view -> Toast.makeText(getApplicationContext(), R.string.toast_message, Toast.LENGTH_LONG).show());
+
+        ((Switch)findViewById(R.id.switch1)).setOnCheckedChangeListener((s, b) ->
+        {
+            Snackbar sb = Snackbar.make(s, getString(R.string.switch_is_now_text) + " " + (b ? getString(R.string.on_text) : getString(R.string.off_text)), Snackbar.LENGTH_LONG);
+            sb.setAction(R.string.undo_text, click -> s.setChecked(!b));
+            sb.show();
+        });
     }
 }
